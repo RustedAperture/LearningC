48061dcf0d1b24e432e053ecacd2c2da6bebed29
Cameron Varley <cam.avarley@gmail.com>
2022-06-30 14:07:13 -0400

    100 Doors

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # My Journey in Learning C
 
-This will be my journey into learning C. My goal is to complete the entirety of the lists found here: [Programming Tasks](https://rosettacode.org/wiki/Category:Programming_Tasks) & [Simple](https://rosettacode.org/wiki/Category:Simple)
+This will be my journey into learning C. My goal is to complete the entirety of the lists found here: [Programming Tasks](https://rosettacode.org/wiki/Category:Programming_Tasks)
 
 I will code for about an hour a day to complete the list. I will mainting a list below showing every challenge/task completed
 
 Each Challenge/task will be in it own folder.
+
+## Completed Tasks
+
+- [100 Doors](https://rosettacode.org/wiki/100_doors)
---
+++ Tasks/1/100 Doors/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Create an array of 100 zeros.
+    int door[100] = { 0 };
+    int pass, check;
+    int i;
+
+    for (pass = 0; pass < 100; ++pass){
+        // Start at pass, this way we will not keep toggling 0
+        for (check = pass; check < 100; check += pass + 1){
+            // Since it is a 0 or 1 we can invert them similar to bools
+            door[check] = !door[check];
+        }
+    }
+
+    // Print the answer that we get from above
+    printf("\nAnswer:   ");
+    for (i = 0; i < 100; i++) {
+        printf("%d", door[i]);
+    }
+
+    // This is the intended solution, printing so I can do a comparison
+    printf("\nSolution: 1001000010000001000000001000000000010000000000001000000000000001000000000000000010000000000000000001");
+
+    return 0;
+}
